4a6b032931bfbc2fe3e8af0fe9df682e47633ee9
harry <zh_harry@163.com>
2022-07-05 08:57:48 +0800

    debug authorizing

--- sparrow-test/src/test/java/com/sparrow/rocketmq/spring/AbstractSpringMQHandler.java
+++ sparrow-test/src/test/java/com/sparrow/rocketmq/spring/AbstractSpringMQHandler.java
@@ -20,11 +20,12 @@ package com.sparrow.rocketmq.spring;
 import com.sparrow.mq.AbstractMQHandler;
 import com.sparrow.mq.MQEvent;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @author by harry
  */
-public abstract class AbstractSpringMQHandler<T extends MQEvent> extends AbstractMQHandler<T> implements BeanNameAware {
+public abstract class AbstractSpringMQHandler<T extends MQEvent> extends AbstractMQHandler<T>  {
     @Override public void setBeanName(String s) {
         this.aware(null, s);
     }
