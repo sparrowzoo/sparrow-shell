0ff3a527bf3b2e775dfd9e15b0caa90559a269e6
harry <zh_harry@163.com>
2022-07-19 09:10:20 +0800

    add ddd entity add ddd ValueObject

---
+++ sparrow-protocol/src/main/java/com/sparrow/protocol/ddd/Entity.java
@@ -0,0 +1,20 @@
+package com.sparrow.protocol.ddd;
+
+public interface Entity<T,ID> {
+
+  /**
+   * Entities compare by identity, not by attributes.
+   *
+   * @param other The other entity.
+   * @return true if the identities are the same, regardles of other attributes.
+   */
+  boolean sameIdentityAs(T other);
+
+  /**
+   * Entities have an identity.
+   *
+   * @return The identity of this entity.
+   */
+  ID identity();
+
+}
