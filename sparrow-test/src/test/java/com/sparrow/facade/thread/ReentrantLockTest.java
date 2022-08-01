caa62e6fa29797138d6d58b17e6df34d05a96899
harry <zh_harry@163.com>
2022-08-01 12:45:17 +0800

    add annotation

--- sparrow-test/src/test/java/com/sparrow/facade/thread/ReentrantLockTest.java
+++ sparrow-test/src/test/java/com/sparrow/facade/thread/ReentrantLockTest.java
@@ -1,62 +1,90 @@
 package com.sparrow.facade.thread;
 
-import java.io.IOException;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockTest {
+    static int b = 1;
+
     public static void main(String[] args) throws InterruptedException {
 
         final ReentrantLock lock = new ReentrantLock(false);
 
-        lock.lock();
-        lock.lock();
         Runnable runnable = () -> {
-                try {
-                    System.out.println(Thread.currentThread().getName() + " 准备拿锁");
-                    lock.lockInterruptibly();
-                    //long ms = new Random().nextInt(500);
-                    System.out.println(Thread.currentThread().getName() + " 获取锁，执行业务逻辑！" + 100 + "ms");
-                   while (true) {
-    //                    if (Thread.currentThread().isInterrupted()) {
-    //                        System.out.println(Thread.currentThread().getName() + " end....");
-    //                        break;
-    //                    }
-                   }
-                } catch (Exception e) {
-                    System.err.println(Thread.currentThread().getName() + "中断...");
-                } finally {
-                    lock.unlock();
-                }
-        };
+            try {
+                System.out.println(Thread.currentThread().getName() + " 准备拿锁");
+                lock.lock();
+                System.out.println(Thread.currentThread().getName() + " 获取锁，执行业务逻辑！" + 100 + "ms");
+                //LockSupport.park();
 
+                System.err.println(Thread.currentThread().getName() + "-" + Thread.currentThread().getState() + "-" + Thread.currentThread().isInterrupted());
+
+                Thread.sleep(Integer.MAX_VALUE);
+//                while (true) {
+//                    if (b == 0) {
+//                        break;
+//                    }
+//                }
+            } catch (Exception e) {
+                System.err.println(Thread.currentThread().getName() + "-" + Thread.currentThread().getState() + "-" + Thread.currentThread().isInterrupted());
+                System.err.println(Thread.currentThread().getName() + "中断...");
+            } finally {
+                lock.unlock();
+            }
+        };
 
         // 创建两个线程
         Thread thread1 = new Thread(runnable, "thread-1");
 //        thread1.setPriority(1);
-      // Thread thread2 = new Thread(runnable, "thread-2");
+        Thread thread2 = new Thread(runnable, "thread-2");
 //        thread2.setPriority(1);
-//        Thread thread3 = new Thread(runnable, "thread-3");
+        Thread thread3 = new Thread(runnable, "thread-3");
 //        thread3.setPriority(1);
-//        Thread thread4 = new Thread(runnable, "thread-4");
+        Thread thread4 = new Thread(runnable, "thread-4");
 //        thread4.setPriority(1);
-//        Thread thread5 = new Thread(runnable, "thread-5");
+        Thread thread5 = new Thread(runnable, "thread-5");
 //        thread5.setPriority(1);
 
         thread1.start();
-        thread1.interrupt();
-        //Thread.sleep(20);
-        //thread2.start();
-//        thread3.start();
-//        thread4.start();
-//        //Thread.sleep(50);
-//        thread5.start();
+        Thread.sleep(20);
+        thread2.start();
+        Thread.sleep(20);
+        thread3.start();
+        Thread.sleep(20);
+        thread4.start();
+        Thread.sleep(50);
+        thread5.start();
 //        try {
 //            System.in.read();
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-        //thread5.interrupt();
-//        thread4.interrupt();
-//        thread1.interrupt();
+
+        Thread monitor = new Thread(new Runnable() {
+            @Override public void run() {
+                while (true) {
+                    System.out.println(thread1.getName() + "-" + thread1.getState() + "-" + thread1.isInterrupted());
+                    System.out.println(thread2.getName() + "-" + thread2.getState() + "-" + thread2.isInterrupted());
+                    System.out.println(thread3.getName() + "-" + thread3.getState() + "-" + thread3.isInterrupted());
+                    System.out.println(thread4.getName() + "-" + thread4.getState() + "-" + thread4.isInterrupted());
+                    System.out.println(thread5.getName() + "-" + thread5.getState() + "-" + thread5.isInterrupted());
+
+                    try {
+                        Thread.sleep(5000L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (b == 1) {
+                        thread1.interrupt();
+                        thread2.interrupt();
+                        thread3.interrupt();
+                        thread4.interrupt();
+                        b = 0;
+                    }
+                }
+            }
+        });
+        monitor.start();
+        Thread.sleep(Integer.MAX_VALUE);
     }
 }
