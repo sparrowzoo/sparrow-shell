664cfbefbaeaa5efca0891b8193bc486614c72bb
harry <zh_harry@163.com>
2022-08-25 09:20:04 +0800

    add action mapping log

---
+++ sparrow-test/src/test/java/com/sparrow/jdk/clinit/MainTest.java
@@ -0,0 +1,7 @@
+package com.sparrow.jdk.clinit;
+
+public class MainTest {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.sparrow.jdk.clinit.Main");
+    }
+}
