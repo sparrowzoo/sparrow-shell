bcf4f5dcf31d70a7e2cb724b1ada80edcc73665f
harry <zh_harry@163.com>
2022-09-05 09:55:26 +0800

    add action mapping log

--- sparrow-test/src/test/java/com/sparrow/facade/thread/visible/MemorySync.java
+++ sparrow-test/src/test/java/com/sparrow/facade/thread/visible/MemorySync.java
@@ -1,6 +1,6 @@
 package com.sparrow.facade.thread.visible;
 
-import java.util.concurrent.TimeUnit;
+import org.apache.poi.ss.formula.functions.T;
 
 public class MemorySync {
     static class ThreadDemo implements Runnable {
@@ -9,23 +9,37 @@ public class MemorySync {
 
         @Override
         public void run() {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             flag = true;
             System.out.println("set flag:" + flag);
+            try {
+                Thread.sleep(Integer.MAX_VALUE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
         public boolean getFlag() {
             return flag;
         }
     }
     public static void main(String[] args) throws InterruptedException {
         ThreadDemo threadDemo = new ThreadDemo();
-        new Thread(threadDemo, "t1").start();
-        while (true) {
-            if (threadDemo.getFlag()) {
-                System.out.println("end task");
-                break;
+        new Thread(new Runnable() {
+            @Override public void run() {
+                while (true) {
+                    if (threadDemo.getFlag()) {
+                        System.out.println("end task");
+                        break;
+                    }
+                }
             }
-        }
+        }).start();
+
+        new Thread(threadDemo, "t1").start();
     }
 }
 
