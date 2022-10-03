b85523b3e603f547ee83ca6f7450b9e2d162dd91
guojian <guojian@365ime.com>
2022-10-03 20:02:42 +0800

    add some util method

--- sparrow-protocol/src/main/java/com/sparrow/protocol/enums/StatusRecord.java
+++ sparrow-protocol/src/main/java/com/sparrow/protocol/enums/StatusRecord.java
@@ -17,6 +17,8 @@
 
 package com.sparrow.protocol.enums;
 
+import java.util.Objects;
+
 /**
  * 记录状态
  *
@@ -58,4 +60,41 @@ public enum StatusRecord {
     }
 
     public final int status;
+
+    /**
+     * 比较状态值是否一致
+     * @param status
+     * @return
+     */
+    public boolean equalsStatus(Integer status){
+        return Objects.equals(this.status,status);
+    }
+
+    /**
+     * 通过status匹配枚举
+     * @param status
+     * @return
+     */
+    public static StatusRecord valueOfStatus(Integer status){
+        if (status == null){
+            return null;
+        }
+        final StatusRecord[] values = StatusRecord.values();
+        for (StatusRecord value : values) {
+            if (value.equalsStatus(status)){
+                return value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 判断状态是否合法
+     * @param status
+     * @return
+     */
+    public static boolean isLegal(Integer status){
+        final StatusRecord statusRecord = valueOfStatus(status);
+        return statusRecord != null;
+    }
 }
