4cc8baae8f1a441f9966f4c01f4d92d962e60216
harry <zh_harry@163.com>
2023-02-02 13:44:05 +0800

    init thymeleaf template

---
+++ sparrow-mvc-thymeleaf/src/main/java/com/sparrow/mvc/result/ThymeleafViewWithModelMethodReturnValueResolverHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sparrow.mvc.result;
+
+import com.sparrow.constant.Config;
+import com.sparrow.mvc.PageSwitchMode;
+import com.sparrow.mvc.ThymeleafEngineUtils;
+import com.sparrow.mvc.result.impl.ViewWithModelMethodReturnValueResolverHandlerImpl;
+import com.sparrow.protocol.constant.Extension;
+import com.sparrow.utility.ConfigUtility;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ThymeleafViewWithModelMethodReturnValueResolverHandler extends ViewWithModelMethodReturnValueResolverHandlerImpl {
+    @Override
+    protected void forward(HttpServletRequest request, HttpServletResponse response,
+        String url) throws  IOException {
+        ThymeleafEngineUtils.forward(request, response, url);
+    }
+
+    @Override
+    protected String assembleUrl(String referer, String defaultSuccessUrl, String url, PageSwitchMode pageSwitchMode,
+        String[] urlArgs) {
+        String assembleUrl = super.assembleUrl(referer, defaultSuccessUrl, url, pageSwitchMode, urlArgs);
+        String pagePrefix = ConfigUtility.getValue(Config.DEFAULT_PAGE_PREFIX, "/template");
+        String extension = ConfigUtility.getValue(Config.DEFAULT_PAGE_EXTENSION, Extension.HTML);
+
+        if (assembleUrl.startsWith(pagePrefix)) {
+            assembleUrl = assembleUrl.substring(pagePrefix.length());
+        }
+        if(assembleUrl.endsWith(extension)) {
+            assembleUrl = assembleUrl.substring(0, assembleUrl.indexOf(extension));
+        }
+        return assembleUrl;
+    }
+}
