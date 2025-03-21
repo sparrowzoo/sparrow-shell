9eb51664166a3ce8dc105d66fca1d930cfcfe218
张立志 <zhanglizhi@tedu.cn>
2025-03-21 08:02:56 +0800

    修改时间误差函数

--- build.sh
+++ build.sh
@@ -1,1 +1,28 @@
+cd sparrow-protocol
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-protocol-dao
+mvn clean install -Dmaven.test.skip
+cd ../sparrow
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-protocol-mq
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-json
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-data-source
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-loader
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-log
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-orm
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-mvc
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-aop
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-markdown
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-rocketmq-client
+mvn clean install -Dmaven.test.skip
+cd ../sparrow-mvc-thymeleaf
 mvn clean install -Dmaven.test.skip
